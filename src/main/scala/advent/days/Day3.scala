75159e39c87740092a745058a5c5ef9bc6b33d97
Alexander Solovyov <xerox.vrn@gmail.com>
2024-12-03 12:38:04 +0300

    Day3

--- src/main/scala/advent/days/Day3.scala
+++ src/main/scala/advent/days/Day3.scala
@@ -5,9 +5,42 @@ import advent.{Solution, Task}
 object Day3 extends Task {
 
   override def solve(input: List[String]): (Solution, Solution) = {
+    val re = "(do)\\(\\)|(don't)\\(\\)|(mul)\\((\\d+),(\\d+)\\)".r
+
+    val ops = input
+      .flatMap(re.findAllMatchIn(_).toList)
+      .map {
+        case re("do", _, _, _, _) => Operation.Do
+        case re(_, "don't", _, _, _) => Operation.Dont
+        case re(_, _, "mul", a, b) => Operation.Mul(a.toInt, b.toInt)
+      }
+
     (
-      () => 0,
-      () => 0
+      () => ops.map {
+        case Operation.Mul(a, b) => (a, b)
+        case _ => (0, 0)
+      }.map(_ * _).sum,
+      
+      () => {
+        case class State(allow: Boolean, sum: Long) {
+          def sum(sum: Long): State = copy(sum = this.sum + sum)
+        }
+
+        ops.foldLeft(State(true, 0)) { (state, op) =>
+            op match {
+              case Operation.Do => state.copy(allow = true)
+              case Operation.Dont => state.copy(allow = false)
+              case Operation.Mul(a, b) => if (state.allow) state.sum(a * b) else state
+            }
+          }
+          .sum
+      }
     )
+  }
+
+  private enum Operation {
+    case Do
+    case Dont
+    case Mul(a: Int, b: Int)
   }
 }
